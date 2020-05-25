e880c04b8276ce54a9b2161eece044515376654f
Oleksandr Fediashov <olfedias@microsoft.com>
2020-05-25 17:44:57 +0200

    working impl

--- packages/fluentui/react-northstar/src/utils/felaRenderer.tsx
+++ packages/fluentui/react-northstar/src/utils/felaRenderer.tsx
@@ -35,14 +35,28 @@ function isPlainObject(val) {
   return val != null && typeof val === 'object' && Array.isArray(val) === false;
 }
 
+function toLowerCaseFirstCharacter(value) {
+  return value.substr(0, 1).toLowerCase() + value.substr(1);
+}
+
+function toUpperCaseFirstCharacter(value) {
+  return value.substr(0, 1).toUpperCase() + value.substr(1);
+}
+
 function camelCasePropertyNonRegex(property) {
-  const a = property
-    .split('-')
-    .map(function(v) {
-      return v.substr(0, 1).toUpperCase() + v.substr(1);
-    })
-    .join('');
-  return a.substr(0, 1).toLowerCase() + a.substr(1);
+  // -ms-transition => ['', 'ms', 'transition']
+  // transition-delay => ['transition', 'delay']
+  // WebkitTransitionDelay => ['WebkitTransitionDelay']
+  const separatedByDash = property.split('-');
+
+  if (separatedByDash.length === 1) {
+    return property;
+  }
+
+  const inPascalCase = separatedByDash.map(toUpperCaseFirstCharacter).join('');
+  const shouldLowerCaseFirstChar = separatedByDash[0] !== '' || separatedByDash[1] === 'ms';
+
+  return shouldLowerCaseFirstChar ? toLowerCaseFirstCharacter(inPascalCase) : inPascalCase;
 }
 
 function generateDeclarationReference(
